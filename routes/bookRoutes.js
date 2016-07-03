5cfcfceb996523f5644ed00958b5efa9f0bbc2ee
samblacklock <samblacklock@me.com>
2016-07-03 18:14:40 +0100

    middleware

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -4,7 +4,7 @@ const express = require('express');
 const Book = require('./../models/bookModel');
 
 const routes = () => {
-  const bookRouter = express.Router()
+  const bookRouter = express.Router();
 
   bookRouter.route('/Books')
     .post((req, res) => {
@@ -27,26 +27,57 @@ const routes = () => {
       });
     });
 
+  bookRouter.use('/Books/:bookId', (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if(err) {
+        res.status(500).send(err);
+      }
+      else if(book) {
+        req.book = book;
+        next();
+      } else {
+        res.status(404).send('not found')
+      }
+    });
+  });
+
   bookRouter.route('/Books/:bookId')
     .get((req, res) => {
-      Book.findById(req.params.bookId, (err, book) => {
-        if (err) res.status(500).send(err);
-
-        res.json(book);
-      });
+      res.json(req.book);
     })
     .put((req, res) => {
-      console.log('put detected');
-      Book.findById(req.params.bookId, (err, book) => {
+      req.book.title = req.body.title;
+      req.book.author = req.body.author ;
+      req.book.genre = req.body.genre;
+      req.book.read = req.body.read;
+
+      req.book.save((err) => {
         if (err) res.status(500).send(err);
+        else res.json(req.book);
+      });
+      res.json(req.book);
+    })
+    .patch((req, res) => {
+      if(req.body._id) {
+        delete req.body._id;
+      }
 
-        book.title = req.body.title;
-        book.author = req.body.author ;
-        book.genre = req.body.genre;
-        book.read = req.body.read;
+      for(let p in req.body) {
+        req.book[p] = req.body[p]
+      }
 
-        book.save();
-        res.json(book);
+      req.book.save((err) => {
+          if (err) res.status(500).send(err);
+          else res.json(req.book);
+      });
+    })
+    .delete((req, res) => {
+      req.book.remove((err) => {
+        if(err) {
+          res.status(500).send(err);
+        } else {
+          res.status(204).send('Remove');
+        }
       });
     });
 
